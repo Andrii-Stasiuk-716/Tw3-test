71612b7429f2a6f6dab700cc08667c8ed3f4b57d
ReactRocket <andrii.stasiuk.work@gmail.com>
2021-09-17 17:09:58 +0300

    first commit234543

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { builder } from '@builder.io/react';
 import { App } from './App';
+
+builder.init('b1f8c11006604e4a9ddf2975c3f19881');
 
 ReactDOM.render(
   <React.StrictMode>
